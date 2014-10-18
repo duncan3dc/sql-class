080d89c96cfc2c771aca0fcf57f0cb324e5434fc
Craig Duncan <git@duncanc.co.uk>
2014-10-18 20:33:47 +0100

    Updated the tests to follow the psr coding standards

--- tests/SqlTest.php
+++ tests/SqlTest.php
@@ -1,12 +1,14 @@
 <?php
 
-class SqlTest extends PHPUnit_Framework_TestCase {
+namespace duncan3dc\SqlClass;
 
+class SqlTest extends \PHPUnit_Framework_TestCase
+{
     private $sql;
 
 
-    public function __construct() {
-
+    public function __construct()
+    {
         $this->sql = new \duncan3dc\SqlClass\Sql([
             "mode"      =>  "sqlite",
             "database"  =>  "/tmp/phpunit.sqlite",
@@ -21,34 +23,32 @@ class SqlTest extends PHPUnit_Framework_TestCase {
     }
 
 
-    public function __destruct() {
-
+    public function __destruct()
+    {
         $this->sql->mode = false;
-
     }
 
 
-    protected function callProtectedMethod($methodName,&$params) {
-
-        $class = new ReflectionClass("\\duncan3dc\\SqlClass\\Sql");
+    protected function callProtectedMethod($methodName, &$params)
+    {
+        $class = new \ReflectionClass(__NAMESPACE__ . "\\Sql");
 
         $method = $class->getMethod($methodName);
 
         $method->setAccessible(true);
 
-        if(is_array($params)) {
-            return $method->invokeArgs($this->sql,$params);
-        } elseif($params) {
-            return $method->invokeArgs($this->sql,[&$params]);
+        if (is_array($params)) {
+            return $method->invokeArgs($this->sql, $params);
+        } elseif ($params) {
+            return $method->invokeArgs($this->sql, [&$params]);
         } else {
             return $method->invokeArgs($this->sql);
         }
-
     }
 
 
-    public function testQuoteChars() {
-
+    public function testQuoteChars()
+    {
         $query = "SELECT field1 `field2` FROM table1";
 
         $modes = [
@@ -57,17 +57,16 @@ class SqlTest extends PHPUnit_Framework_TestCase {
             "odbc"     =>  "SELECT field1 \"field2\" FROM table1",
             "sqlite"   =>  "SELECT field1 `field2` FROM table1",
         ];
-        foreach($modes as $mode => $check) {
+        foreach ($modes as $mode => $check) {
             $this->sql->mode = $mode;
-            $this->callProtectedMethod("quoteChars",$query);
-            $this->assertEquals($check,$query);
+            $this->callProtectedMethod("quoteChars", $query);
+            $this->assertEquals($check, $query);
         }
-
     }
 
 
-    public function testQuoteCharsBackcheck() {
-
+    public function testQuoteCharsBackcheck()
+    {
         $this->sql->mode = "mssql";
 
         $check = "SELECT a.field1, SUM(b.field2) [field2], SUM(c.field3) [field3], 'field4' [field4] FROM {table1} a
@@ -99,15 +98,14 @@ class SqlTest extends PHPUnit_Framework_TestCase {
                 JOIN {table2} b ON b.field1=a.field1
                 JOIN {table2} b ON b.field1=a.field1
                 JOIN {table2} b ON b.field1=a.field1";
-        $this->callProtectedMethod("quoteChars",$query);
+        $this->callProtectedMethod("quoteChars", $query);
 
-        $this->assertEquals($check,$query);
-
+        $this->assertEquals($check, $query);
     }
 
 
-    public function testFunctions() {
-
+    public function testFunctions()
+    {
         $query = "SELECT IFNULL(field1,0), SUBSTR(field1,5,3) FROM table1";
 
         $modes = [
@@ -116,18 +114,16 @@ class SqlTest extends PHPUnit_Framework_TestCase {
             "odbc"     =>  "SELECT IFNULL(field1,0), SUBSTRING(field1,5,3) FROM table1",
             "mysql"    =>  "SELECT IFNULL(field1,0), SUBSTRING(field1,5,3) FROM table1",
         ];
-        foreach($modes as $mode => $check) {
+        foreach ($modes as $mode => $check) {
             $this->sql->mode = $mode;
-            $this->callProtectedMethod("functions",$query);
-            $this->assertEquals($check,$query);
+            $this->callProtectedMethod("functions", $query);
+            $this->assertEquals($check, $query);
         }
-
-
     }
 
 
-    public function testLimit() {
-
+    public function testLimit()
+    {
         $modes = [
             "mysql"    =>  ["SELECT * FROM table1 FETCH FIRST 10 ROWS ONLY", "SELECT * FROM table1 \nLIMIT 10\n"],
             "sqlite"   =>  ["SELECT * FROM table1
@@ -135,18 +131,16 @@ class SqlTest extends PHPUnit_Framework_TestCase {
                         \nLIMIT 10\n"],
             "odbc"     =>  ["SELECT * FROM table1 LIMIT 1", "SELECT * FROM table1 \nFETCH FIRST 1 ROWS ONLY\n"],
         ];
-        foreach($modes as $mode => list($query,$check)) {
+        foreach ($modes as $mode => list($query, $check)) {
             $this->sql->mode = $mode;
-            $this->callProtectedMethod("limit",$query);
-            $this->assertEquals($check,$query);
+            $this->callProtectedMethod("limit", $query);
+            $this->assertEquals($check, $query);
         }
-
-
     }
 
 
-    public function testTableNames() {
-
+    public function testTableNames()
+    {
         $modes = [
             "mysql"    =>  ["SELECT * FROM {table1}", "SELECT * FROM `test1`.`table1`"],
             "mssql"    =>  ["SELECT * FROM {table1}
@@ -157,17 +151,16 @@ class SqlTest extends PHPUnit_Framework_TestCase {
                             LEFT OUTER JOIN {nosuchtable} ON field2a=field1a", "SELECT * FROM `test1`.`table1`
                             LEFT OUTER JOIN nosuchtable ON field2a=field1a"],
         ];
-        foreach($modes as $mode => list($query,$check)) {
+        foreach ($modes as $mode => list($query, $check)) {
             $this->sql->mode = $mode;
-            $this->callProtectedMethod("tableNames",$query);
-            $this->assertEquals($check,$query);
+            $this->callProtectedMethod("tableNames", $query);
+            $this->assertEquals($check, $query);
         }
-
     }
 
 
-    public function testQueryParams() {
-
+    public function testQueryParams()
+    {
         $checkQuery = "SELECT * FROM test
             WHERE field1=?
                 AND field2 IN ((?,?,?))
@@ -206,25 +199,24 @@ class SqlTest extends PHPUnit_Framework_TestCase {
             ORDER BY field2";
         $params = [
             "marker1",
-            ["marker2a","marker2b","marker2c"],
+            ["marker2a", "marker2b", "marker2c"],
             "marker3",
             ["marker4a"],
-            ["marker5a","marker5b","marker5c"],
+            ["marker5a", "marker5b", "marker5c"],
             ["marker6a"],
             ["assoc" => "marker7a"],
-            ["assoc1" => "marker8a","assoc2" => "marker8b"],
+            ["assoc1" => "marker8a", "assoc2" => "marker8b"],
         ];
 
-        $args = [&$query,&$params];
-        $this->callProtectedMethod("paramArrays",$args);
-        $this->assertEquals($checkQuery,$query);
-        $this->assertEquals($checkParams,$params);
-
+        $args = [&$query, &$params];
+        $this->callProtectedMethod("paramArrays", $args);
+        $this->assertEquals($checkQuery, $query);
+        $this->assertEquals($checkParams, $params);
     }
 
 
-    public function testPrepareQuery() {
-
+    public function testPrepareQuery()
+    {
         $this->sql->mode = "mssql";
 
         $query = "SELECT * FROM test WHERE field1=? ORDER BY field2";
@@ -234,15 +226,14 @@ class SqlTest extends PHPUnit_Framework_TestCase {
 
         $check = "SELECT * FROM test WHERE field1='Test''s' ORDER BY field2";
 
-        $args = [&$query,&$params];
-        $query = $this->callProtectedMethod("prepareQuery",$args);
-        $this->assertEquals($check,$query);
-
+        $args = [&$query, &$params];
+        $query = $this->callProtectedMethod("prepareQuery", $args);
+        $this->assertEquals($check, $query);
     }
 
 
-    public function testPrepareQueryTypes() {
-
+    public function testPrepareQueryTypes()
+    {
         $this->sql->mode = "odbc";
         $this->sql->allowNulls = false;
 
@@ -279,18 +270,17 @@ class SqlTest extends PHPUnit_Framework_TestCase {
         ];
 
         $args = [&$params];
-        $this->callProtectedMethod("convertNulls",$args);
+        $this->callProtectedMethod("convertNulls", $args);
 
-        $args = [&$query,&$params];
-        $query = $this->callProtectedMethod("prepareQuery",$args);
+        $args = [&$query, &$params];
+        $query = $this->callProtectedMethod("prepareQuery", $args);
 
-        $this->assertEquals($check,$query);
-
+        $this->assertEquals($check, $query);
     }
 
 
-    public function testPrepareQueryNulls() {
-
+    public function testPrepareQueryNulls()
+    {
         $this->sql->mode = "odbc";
         $this->sql->allowNulls = true;
 
@@ -327,102 +317,98 @@ class SqlTest extends PHPUnit_Framework_TestCase {
         ];
 
         $args = [&$params];
-        $this->callProtectedMethod("convertNulls",$args);
+        $this->callProtectedMethod("convertNulls", $args);
 
-        $args = [&$query,&$params];
-        $query = $this->callProtectedMethod("prepareQuery",$args);
+        $args = [&$query, &$params];
+        $query = $this->callProtectedMethod("prepareQuery", $args);
 
-        $this->assertEquals($check,$query);
-
+        $this->assertEquals($check, $query);
     }
 
 
-    public function testSelectFields() {
-
+    public function testSelectFields()
+    {
         $check = "''";
         $result = $this->sql->selectFields("");
-        $this->assertEquals($check,$result);
+        $this->assertEquals($check, $result);
 
         $check = "''";
         $result = $this->sql->selectFields([]);
-        $this->assertEquals($check,$result);
+        $this->assertEquals($check, $result);
 
         $this->sql->mode = "mysql";
         $check = "`field1`, `field2`";
-        $result = $this->sql->selectFields(["field1","field2"]);
-        $this->assertEquals($check,$result);
+        $result = $this->sql->selectFields(["field1", "field2"]);
+        $this->assertEquals($check, $result);
 
         $this->sql->mode = "mssql";
         $check = "[field1]";
         $result = $this->sql->selectFields(["field1"]);
-        $this->assertEquals($check,$result);
+        $this->assertEquals($check, $result);
 
         $check = "''";
         $result = $this->sql->selectFields(true);
-        $this->assertEquals($check,$result);
+        $this->assertEquals($check, $result);
 
         $check = "''";
         $result = $this->sql->selectFields(false);
-        $this->assertEquals($check,$result);
+        $this->assertEquals($check, $result);
 
         $check = "*";
         $result = $this->sql->selectFields("*");
-        $this->assertEquals($check,$result);
+        $this->assertEquals($check, $result);
 
         $check = "field1, field2, field3";
         $result = $this->sql->selectFields("field1, field2, field3");
-        $this->assertEquals($check,$result);
-
+        $this->assertEquals($check, $result);
     }
 
 
-    public function testOrderBy() {
-
+    public function testOrderBy()
+    {
         $check = "ORDER BY `field1`";
         $query = $this->sql->orderBy("field1");
-        $this->assertEquals($check,$query);
+        $this->assertEquals($check, $query);
 
         $check = "ORDER BY `field1`, `field2`, `field3`";
         $query = $this->sql->orderBy("field1, field2, field3");
-        $this->assertEquals($check,$query);
+        $this->assertEquals($check, $query);
 
         $check = "";
         $query = $this->sql->orderBy("");
-        $this->assertEquals($check,$query);
+        $this->assertEquals($check, $query);
 
         $check = "";
         $query = $this->sql->orderBy([]);
-        $this->assertEquals($check,$query);
+        $this->assertEquals($check, $query);
 
         $check = "ORDER BY `field1`, `field2`, `field3`";
-        $query = $this->sql->orderBy(["field1","field2","field3"]);
-        $this->assertEquals($check,$query);
+        $query = $this->sql->orderBy(["field1", "field2", "field3"]);
+        $this->assertEquals($check, $query);
 
         $check = "ORDER BY CASE WHEN field1>0 THEN 0 ELSE 1 END, `field2`";
         $query = $this->sql->orderBy("CASE WHEN field1>0 THEN 0 ELSE 1 END, field2");
-        $this->assertEquals($check,$query);
-
+        $this->assertEquals($check, $query);
     }
 
 
-    public function testModifyQuery() {
-
+    public function testModifyQuery()
+    {
         $this->sql->mode = "mssql";
 
         $check = "ORDER BY 'basic `protection` against {special} [characters] withing strings', [field1]";
         $query = "ORDER BY 'basic `protection` against {special} [characters] withing strings', `field1`";
 
-        $this->callProtectedMethod("quoteChars",$query);
-        $this->assertEquals($check,$query);
+        $this->callProtectedMethod("quoteChars", $query);
+        $this->assertEquals($check, $query);
 
-        $this->callProtectedMethod("tableNames",$query);
-        $this->assertEquals($check,$query);
-
+        $this->callProtectedMethod("tableNames", $query);
+        $this->assertEquals($check, $query);
     }
 
 
-    public function testNamedParams() {
-
+    public function testNamedParams()
+    {
         $this->sql->mode = "sqlite";
 
         $check = "SELECT field1 FROM table1 a
@@ -447,17 +433,16 @@ class SqlTest extends PHPUnit_Framework_TestCase {
             "field2"    =>  "two",
         ];
 
-        $args = [&$query,&$params];
-        $this->callProtectedMethod("namedParams",$args);
-        $query = $this->callProtectedMethod("prepareQuery",$args);
+        $args = [&$query, &$params];
+        $this->callProtectedMethod("namedParams", $args);
+        $query = $this->callProtectedMethod("prepareQuery", $args);
 
-        $this->assertEquals($check,$query);
-
+        $this->assertEquals($check, $query);
     }
 
 
-    public function testEmptyStrings() {
-
+    public function testEmptyStrings()
+    {
         $this->sql->mode = "sqlite";
         $this->sql->definitions([
             "table1"    =>  "database1",
@@ -476,29 +461,25 @@ class SqlTest extends PHPUnit_Framework_TestCase {
                 WHERE a.field1='TEST'";
 
         $args = [&$query];
-        $this->callProtectedMethod("tableNames",$args);
-        $this->assertEquals($check,$query);
-
+        $this->callProtectedMethod("tableNames", $args);
+        $this->assertEquals($check, $query);
     }
 
 
-    public function testStrictWhere() {
-
+    public function testStrictWhere()
+    {
         $this->sql->mode = "mysql";
 
         $check = "`field1` IN(?,?) ";
-        $params = [0,1];
+        $params = [0, 1];
 
         $where = [
-            "field1"    =>  [0,1],
+            "field1"    =>  [0, 1],
         ];
 
         $null = [];
-        $args = [&$where,&$null];
-        $query = $this->callProtectedMethod("where",$args);
-        $this->assertEquals($check,$query);
-
+        $args = [&$where, &$null];
+        $query = $this->callProtectedMethod("where", $args);
+        $this->assertEquals($check, $query);
     }
-
-
 }
